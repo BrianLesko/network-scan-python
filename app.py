ebc17da220a098ae9bc928e4e4c6bc7dc41b04a0
Brian Lesko <brianjosephlesko@gmail.com>
2024-04-09 21:38:00 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,72 @@
+# Brian Lesko
+# 4/9/2024 
+# Scan your local wifi network and display the devices that are connected to it.
+
+import subprocess
+import re
+import netifaces
+
+def get_ip_address():
+    #Retrieve the local IP address of the computer on macOS.
+    #:return: str - The IP address, or a message if not found.
+    result = subprocess.run("ifconfig", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    interfaces = ['en0', 'en1', 'en2', 'en3', 'en4', 'en5']
+    for interface in interfaces:
+        match = re.search(rf'{interface}:.*?inet (\d+\.\d+\.\d+\.\d+)', result.stdout, re.DOTALL)
+        if match:
+            return match.group(1)
+    return "No external IP address found."
+
+import subprocess
+import re
+
+def get_subnet_mask(interface='en0'):
+    addrs = netifaces.ifaddresses(interface)
+    return addrs[netifaces.AF_INET][0]['netmask']
+
+subnet_mask = get_subnet_mask()
+print(f"Subnet Mask: {subnet_mask}")
+
+ip_address = get_ip_address()
+print(f"IP address: {ip_address}")
+
+def calculate_broadcast_address(ip, subnet):
+    #Calculate the broadcast address from an IP address and subnet mask.
+    ip_binary = ''.join(format(int(octet), '08b') for octet in ip.split('.'))
+    subnet_binary = ''.join(format(int(octet), '08b') for octet in subnet.split('.'))
+    broadcast_binary = ''.join(ip_bit if subnet_bit == '1' else '1' 
+                               for ip_bit, subnet_bit in zip(ip_binary, subnet_binary))
+    broadcast_address = '.'.join(str(int(broadcast_binary[i:i + 8], 2)) for i in range(0, 32, 8))
+    return broadcast_address
+
+broadcast_address = calculate_broadcast_address(ip_address, subnet_mask)
+print(f"Broadcast Address: {broadcast_address}")
+
+def ping_broadcast(address, timeout):
+    #Pings the specified address with a timeout.
+    try:
+        return subprocess.check_output(['ping', '-c', '3', address], text=True, timeout=timeout)
+    except subprocess.TimeoutExpired:
+        return f"Timeout after {timeout} seconds."
+    except subprocess.CalledProcessError:
+        return "Ping failed."
+
+print(ping_broadcast(broadcast_address, 5))
+
+def get_arp_table():
+    try:
+        arp_output = subprocess.check_output(['arp', '-a'], text=True)
+        return [line for line in arp_output.splitlines() if 'permanent' not in line]
+    except subprocess.CalledProcessError:
+        return []
+
+for line in get_arp_table():
+    match = re.search(r'(\?\S*) \(([\d\.]+)\) at ([\w:]+) on (\w+)', line)
+    if match:
+        hostname, ipv4, mac, interface = match.groups()
+        # Replace '?' with a placeholder if hostname is not available
+        hostname = hostname if hostname != '?' else 'N/A'
+        print(f"{ipv4} is assigned to {mac}, has name {hostname}, on the {interface} network")
+    else:
+        print("Line could not be parsed:", line)
+    
---
+++ readme.md
@@ -0,0 +1,66 @@
+
+# Network Scan for LAN networks in Python
+discover other devices on a LAN network, along with your own IP, subnet, and the broadcast address. 
+Written to work on Apple Computers only. 
+
+&nbsp;
+
+<div align="center"><img src="docs/preview.gif" width="800"></div>
+
+&nbsp;
+
+## Software Dependencies
+
+This code uses the following libraries:
+- `netifaces`: for retrieving the subnet only
+
+&nbsp;
+
+## Hardware Dependencies
+- `An Apple or Mac Computer`
+
+## Usage
+```
+pip install netifaces
+python app.py
+```
+
+&nbsp;
+
+## Topics 
+```
+Python | Internet IP 
+Mechanical and Robotics engineer
+```
+&nbsp;
+
+<hr>
+
+&nbsp;
+
+<div align="center">
+
+
+
+╭━━╮╭━━━┳━━┳━━━┳━╮╱╭╮        ╭╮╱╱╭━━━┳━━━┳╮╭━┳━━━╮
+┃╭╮┃┃╭━╮┣┫┣┫╭━╮┃┃╰╮┃┃        ┃┃╱╱┃╭━━┫╭━╮┃┃┃╭┫╭━╮┃
+┃╰╯╰┫╰━╯┃┃┃┃┃╱┃┃╭╮╰╯┃        ┃┃╱╱┃╰━━┫╰━━┫╰╯╯┃┃╱┃┃
+┃╭━╮┃╭╮╭╯┃┃┃╰━╯┃┃╰╮┃┃        ┃┃╱╭┫╭━━┻━━╮┃╭╮┃┃┃╱┃┃
+┃╰━╯┃┃┃╰┳┫┣┫╭━╮┃┃╱┃┃┃        ┃╰━╯┃╰━━┫╰━╯┃┃┃╰┫╰━╯┃
+╰━━━┻╯╰━┻━━┻╯╱╰┻╯╱╰━╯        ╰━━━┻━━━┻━━━┻╯╰━┻━━━╯
+  
+
+
+&nbsp;
+
+
+<a href="https://twitter.com/BrianJosephLeko"><img src="https://raw.githubusercontent.com/BrianLesko/BrianLesko/f7be693250033b9d28c2224c9c1042bb6859bfe9/.socials/svg-white/x-logo-white.svg" width="30" alt="X Logo"></a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="https://github.com/BrianLesko"><img src="https://raw.githubusercontent.com/BrianLesko/BrianLesko/f7be693250033b9d28c2224c9c1042bb6859bfe9/.socials/svg-white/github-mark-white.svg" width="30" alt="GitHub"></a> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="https://www.linkedin.com/in/brianlesko/"><img src="https://raw.githubusercontent.com/BrianLesko/BrianLesko/f7be693250033b9d28c2224c9c1042bb6859bfe9/.socials/svg-white/linkedin-icon-white.svg" width="30" alt="LinkedIn"></a>
+
+follow all of these for pizza :)
+
+</div>
+
+
+&nbsp;
+
+
